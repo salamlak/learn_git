e44e2c638dcaf1d8386a9d988fe5f24085ac387f
salamlak <70778406+salamlak@users.noreply.github.com>
2022-08-18 18:48:02 +0200

    my first commit

---
+++ bash/run.sh
@@ -0,0 +1,11 @@
+ls
+echo "hello world"
+# print my name is salama in file me,txt
+echo "my name is salama" > me.txt
+#show me that file
+cat me.txt
+
+
+
+
+
---
+++ bash/run1.sh
@@ -0,0 +1,7 @@
+echo "docker login --username=$username --password=$password"
+echo "docker build ......."
+echo "docker push"
+
+#you musn't write credentials in bash scripts you just need to export it by using command 
+#export username=ahmedsalama
+#export password=123456
